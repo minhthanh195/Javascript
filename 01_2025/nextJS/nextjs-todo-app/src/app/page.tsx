62c94c1d47081d1bef3cbe1152a8fc3686a20803
Mingtaing <blueskyalone199@gmail.com>
2025-04-02 17:04:05 +0700

    update code

--- 01_2025/nextJS/nextjs-todo-app/src/app/page.tsx
+++ 01_2025/nextJS/nextjs-todo-app/src/app/page.tsx
@@ -1,79 +1,77 @@
-'use client';
+"use client";
 
-import { ChangeEvent, FormEvent, useState} from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Todo } from "../types/todo";
 import TodoItem from "../components/TodoItem";
 import Clock from "@/components/test";
-
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 export default function Home() {
-  const [todos, setTodos] = useState<Todo[]>([
-    {id : 1, text: 'Next.js', completed: false},
-    {id : 2, text: 'TypeScript', completed: false},
-    {id : 3, text: 'Create todo app', completed: false}
-  ]);
-  const [newTodo, setNewTodo] = useState<string>('');
-
+  const [newTodo, setNewTodo] = useState<string>("");
   const [show, setShow] = useState<boolean>(true);
+  
+  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', [
+    {id: 1, text: 'hello', completed: false},
+    {id: 2, text: 'hello2', completed: false},
+    {id: 3, text: 'hello3', completed: false}
+  ]);
 
-  const handleChangeInput = (e : ChangeEvent<HTMLInputElement>) => {
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setNewTodo(e.target.value);
-  }
+  };
 
-  const handleSubmitForm = (e : FormEvent) => {
+  const handleSubmitForm = (e: FormEvent) => {
     e.preventDefault();
 
-    if(newTodo.trim() === '') return;
+    if (newTodo.trim() === "") return;
 
-    const newItem : Todo = {
+    const newItem: Todo = {
       id: Date.now(),
       text: newTodo,
-      completed : false,
+      completed: false,
     };
-    
+
     setTodos([...todos, newItem]);
-    setNewTodo('');
-  }
+    setNewTodo("");
+  };
 
   const toggleCompleted = (id: number) => {
-    const updateTodo = todos.map((todo) => 
-      todo.id === id ? {...todo, completed: !todo.completed} : todo
-    )
+    const updateTodo = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
 
     setTodos(updateTodo);
-  }
+  };
 
   const deleteTodo = (id: number) => {
-    const updateTodo = todos.filter((todo) => 
-      todo.id !== id 
-    )
+    const updateTodo = todos.filter((todo) => todo.id !== id);
 
     setTodos(updateTodo);
-  }
+  };
 
   return (
     <>
-      <h1>To-do App</h1>
+      <h1 className="text-3xl font-bold underline">To-do App</h1>
 
       <ul>
         {todos.map((todo) => (
           <TodoItem
             key={todo.id}
-            todo = {todo}
+            todo={todo}
             onToggle={toggleCompleted}
             onDelete={deleteTodo}
           />
         ))}
       </ul>
 
-      <form onSubmit={handleSubmitForm} style={{marginBottom: '20px'}}>
-        <input 
+      <form onSubmit={handleSubmitForm} style={{ marginBottom: "20px" }}>
+        <input
           type="text"
           value={newTodo}
           onChange={handleChangeInput}
           placeholder="add work here"
-          style={{padding : '8px', width: '300px'}}
+          style={{ padding: "8px", width: "300px" }}
         />
-        <button type="submit" style={{marginLeft: '10px', padding: '8px'}}>
+        <button type="submit" style={{ marginLeft: "10px", padding: "8px" }}>
           Add
         </button>
       </form>
@@ -83,6 +81,7 @@ export default function Home() {
         {show && <Clock />}
       </div>
 
+      
     </>
   );
 }
